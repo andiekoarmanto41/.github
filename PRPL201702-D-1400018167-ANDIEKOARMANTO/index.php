3c9eb6aeb3207c8ccb4b56da07658888f2bad9bc
andiekoarmanto41 <andi1400018167@webmail.uad.ac.id>
2018-06-04 21:48:31 +0700

    first comit

---
+++ PRPL201702-D-1400018167-ANDIEKOARMANTO/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    	<title>SW Hotel</title>
+<link href="stylesheet.css" rel="stylesheet" type="text/css" />
+<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+</head>
+<body>
+	<div class="main_container" >
+        <a href="#"><img src="images/logo.png" align="left" /></a>
+            <div id="headr_img">
+                <span id="main_nav"> 
+                	   	<a href="#">Home</a> |  
+						<a href="catalogue.php">Accomodations</a> |  
+                        <a href="reserve.php">Reservations</a> |  
+                        <a href="contact.php">Contact Us</a>
+                </span>
+            </div>
+   <div id="content_container">
+<div id="welcome">
+                	<p id="home_header">
+						Pelayanan berkualitas dan harga yang nyaman...
+                    </p>
+                    <p>Kami mengundang anda untuk singgah di hotel kami, dengan tempat yang ideal untuk menginap.</p>
+                    <p>Dengan lingkungan yang ramah, dan mudahnya akses transportasi, sangat memanjakan anda apabila singgah di SW Hotel.</p>
+<p id="home_header">Kami Menawarkan:</p>
+    <ul>
+        <li><i class="icon-li icon-ok"></i>Wireless Internet Access</li>
+        <li>Cable TV channels</li>
+        <li>Air conditioning or fans</li>
+        <li>Clean rooms</li>
+    </ul>
+       	   </div>
+           <span id="img_shadow"><img src="images/1011_n.png"/></span>
+                      <div class="call_to_action" id="grn"><a href="reserve.php">Reserve Now</a></div>
+                      <div class="call_to_action" id="orn"><a href="catalogue.php">View Rooms</a></div>
+      </div>
+         <div id="footer_container">
+         	Copyright © 2018 SW Hotel
+      </div>
+</div>
+</body>
+</html>
